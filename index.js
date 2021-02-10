02002d9d4e84aa80c0ea639d9032276666e1d956
Elisha Jobe <elishakjobe@gmail.com>
2021-02-10 00:33:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,9 +26,9 @@ function divide(a, b) {
 var answer = a / b;
 return answer;
 }
-function increment(+n) {
-return n;
+function increment(n) {
+return ++n;
 }
-function decrement(-n) {
-return n;
+function decrement(n) {
+return --n;
 }
