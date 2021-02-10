dfc9dbc882851bfbf49b04d90a22c5785921c2af
Elisha Jobe <elishakjobe@gmail.com>
2021-02-10 00:14:13 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,28 @@
+function add() {
+
+}
+function subtract() {
+
+}
+function multiply () {
+
+}
+function divide () {
+
+}
+function add(a, b) {
+  var answer = a + b;
+  return answer;
+}
+function subtract(b, a) {
+  var answer = b - a;
+  return answer;
+}
+function multiply(a, b) {
+  var answer = a * b;
+  return answer;
+}
+function divide(a, b) {
+var answer = a / b;
+return answer;
+}
